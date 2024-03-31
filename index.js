8d8d3489e6a7f88e24023147d689a14dc114a5be
Wol <woldut101@icloud.com>
2024-03-31 23:09:48 +0300

    Adds Phase-0 Intro-to-JS-2-Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+  
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+  
+function removeLastCat() {
+    // Using slice() to create a new array without the last cat
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    // Using shift() to remove the first cat and return a new array
+    // Create a copy of the original array
+    // Remove the first cat
+    return cats.slice(1);
+}
+
+
